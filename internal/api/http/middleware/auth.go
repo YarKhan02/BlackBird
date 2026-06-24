9dced64261c8c255ca3c73d9b911a77e6088b7ff
YarKhan02 <yarkhan025@gmail.com>
2026-06-24 17:56:08 +0500

    i hope it works

--- internal/api/http/middleware/auth.go
+++ internal/api/http/middleware/auth.go
@@ -39,7 +39,7 @@ func Auth(tokenSvc *token.Service, blocklist *redis.Blocklist) func(http.Handler
 			if blocklist != nil && claims.ID != "" {
 				blocked, err := blocklist.Contains(r.Context(), claims.ID)
 				if err != nil || blocked {
-					fmt.Printf("failed to validate access token: %v\n", err)
+					fmt.Printf("failed to nlovklist or claims.ID validate access token: %v\n", err)
 					http.Error(w, err.Error(), http.StatusUnauthorized)
 					return
 				}
